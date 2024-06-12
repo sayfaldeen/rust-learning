7ec952a6222e3d5817d9c94c1374b4e2e00537b1
sayfaldeen <50063071+sayfaldeen@users.noreply.github>
2024-06-12 19:58:35 -0400

    Added some additional changes to include match expressions and some neater syntax

--- enums2/src/main.rs
+++ enums2/src/main.rs
@@ -9,16 +9,17 @@ enum ProductType{
     Food,
     Clothing,
     Electronic,
-    Book
+    Book,
+    Medicine
 }
 
 impl ProductType {
     fn clearance_factor(&self) -> f64 {
         match self {
-            ProductType::Food => 0.8, // 20% discount
+            Self::Food | Self::Medicine => 0.8, // 20% discount
             ProductType::Electronic => 0.75, // 25% discount
             _ => 0.65 // 35% discount
-        }
+        } // Self and ProductType are the same here; Self is cleaner though
     }
 }
 
@@ -34,11 +35,13 @@ struct Product {
 impl Product {
     fn price(&self) -> f64 {
 
-        if self.clearance { self.base_price * self.product_type.clearance_factor() } // 40% sale for clearance items
-        else { self.base_price }
+        match self.clearance {
+            true => self.base_price * self.product_type.clearance_factor(),
+            false => self.base_price
+        }
     }
-
 }
+
 
 fn main() {
     // Construct a product
@@ -55,7 +58,7 @@ fn main() {
     let apple = Product{
         name:"Granny Smith apple".to_string(),
         product_type:ProductType::Food,
-        base_price: 9.99,
+        base_price: 10.0,
         items_remaining: 10,
         clearance: true
     };
